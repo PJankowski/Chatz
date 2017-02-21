7647f56fd58657df0a6dc0db10e12d32e3682a4a
Paul Jankowski <PJankowski25@gmail.com>
2017-02-20 19:47:21 -0500

    Added cssnext and removed autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('postcss-cssnext')({
+      features: {
+        customProperties: {
+          variables: {
+            text: '#ebebff',
+            backgroundColor: '#282b34',
+            orange: '#dd672c',
+            red: '#e26b73',
+            green: '#6ac38a',
+            yellow: '#e6c176',
+          },
+        },
+      },
+    }),
   ],
 };
