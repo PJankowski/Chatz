21bde8f5f895a1e7c2a23d08332206dba90d88de
Paul Jankowski <PJankowski25@gmail.com>
2017-02-22 22:06:00 -0500

    Disabling DB Error logs for Heroku

--- server/db.js
+++ server/db.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import serverConfig from './config';
-import writeConnectionError from './handlers/db_handler';
+// import writeConnectionError from './handlers/db_handler';
 
 export default function DB() {
   mongoose.Promise = global.Promise;
 
   mongoose.connect(serverConfig.mongoUri, (err) => {
     if (err) {
-      writeConnectionError(err);
+      // writeConnectionError(err);
+      console.log(err);
     }
   });
 }
