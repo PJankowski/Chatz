d173d4dbba90aa8cff6749406770d7c6234706bb
Paul Jankowski <PJankowski25@gmail.com>
2017-02-22 22:15:01 -0500

    Hopefully this will allow logging

--- server/db.js
+++ server/db.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 import serverConfig from './config';
-// import writeConnectionError from './handlers/db_handler';
+import writeConnectionError from './handlers/db_handler';
 
 export default function DB() {
   mongoose.Promise = global.Promise;
 
   mongoose.connect(serverConfig.mongoUri, (err) => {
     if (err) {
-      // writeConnectionError(err);
-      console.log(err);
+      writeConnectionError(err);
     }
   });
 }
--- server/handlers/db_handler.js
+++ server/handlers/db_handler.js
@@ -1,11 +1,11 @@
-// import fs from 'fs';
-// import path from 'path';
-// import moment from 'moment';
-//
-// export default function writeConnectionError(error) {
-//   const stream = fs.createWriteStream(path.join(__dirname, '../logs/database.log'), { flags: 'a' });
-//   stream.once('open', () => {
-//     stream.write(`[${moment().format('MM/D/YY, HH:MM:SS')}] ${JSON.stringify(error)}\n`);
-//     stream.close();
-//   });
-// }
+import fs from 'fs';
+import path from 'path';
+import moment from 'moment';
+
+export default function writeConnectionError(error) {
+  const stream = fs.createWriteStream(path.join(__dirname, '../logs/database.log'), { flags: 'a' });
+  stream.once('open', () => {
+    stream.write(`[${moment().format('MM/D/YY, HH:MM:SS')}] ${JSON.stringify(error)}\n`);
+    stream.close();
+  });
+}
