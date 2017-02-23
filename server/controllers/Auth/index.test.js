08139fd459b3bcf55004ea817a26ebca90ce4821
Paul Jankowski <PJankowski25@gmail.com>
2017-02-22 23:52:56 -0500

    Started writing test

--- server/controllers/Auth/index.test.js
+++ server/controllers/Auth/index.test.js
@@ -3,26 +3,46 @@ import request from 'supertest';
 
 const api = request('localhost:8080');
 
-describe('Auth suite', () => {
-  it('Logs a user in', () => {
+describe('Login suite', () => {
+  it('Logs a user in', (done) => {
     api
-      .post('/api/login', { username: 'PJankowski25', password: 'Payton15'})
+      .post('/api/login')
+      .type('form')
+      .send({ username: 'PJankowski25', password: 'Payton15' })
       .set('Accept', 'application/json')
-      .expect('Content-Type', '/json/')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-      });
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200, done);
   });
 
-  it('Fails at logging a user in', () => {
+  it('Fails at logging a user in', (done) => {
     api
-      .post('/api/login', { username: 'test', password: 'one'})
+      .post('/api/login')
+      .type('form')
+      .send({ username: 'PJankowski', password: 'weiufhewuihf' })
       .set('Accept', 'application/json')
-      .expect('Content-Type', '/json/')
-      .expect(403)
-      .end((err, res) => {
-        if (err) throw err;
-      });
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(403, done);
+  });
+});
+
+describe('Signup Suite', () => {
+  it('Signs a user up', (done) => {
+    api
+      .post('/api/signup')
+      .type('form')
+      .send({ username: 'a', password: 'b'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200, done);
+  });
+
+  it('Fails at signing a user up', (done) => {
+    api
+      .post('/api/signup')
+      .type('form')
+      .send({ username: 'a', password: 'b'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(403, done);
   });
 });
